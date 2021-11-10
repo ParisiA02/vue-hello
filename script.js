06fd935219e03e7bbb4f9d08882bef836e57a907
Andrea Parisi <andreaparisi4502@gmail.com>
2021-11-10 15:33:02 +0100

    commit exercise

---
+++ script.js
@@ -0,0 +1,8 @@
+let app = new Vue({
+    el: "#root",
+    data:{
+        messaggio: "Hello World!",
+        img: "https://randomwordgenerator.com/img/picture-generator/57e8d64b435aaf14f1dc8460962e33791c3ad6e04e507440752f72d7924fc4_640.jpg"
+    }
+});
+
